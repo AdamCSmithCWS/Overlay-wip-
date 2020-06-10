c5da5648232231850ab507013341f4d7d3e5f366
Adam C Smith <adam.smith2@canada.ca>
2020-06-10 08:53:48 -0400

    some suggestions for ggplot, union, and bbsBayes in CRAN

--- CCS Shapefile Overlay.R
+++ CCS Shapefile Overlay.R
@@ -1,8 +1,10 @@
 #requires Rtools
 #requires JAGS
 
-install.packages("devtools")
-devtools::install_github("BrandonEdwards/bbsBayes", ref = "v2.0.0") 
+# install.packages("devtools")
+# devtools::install_github("BrandonEdwards/bbsBayes", ref = "v2.0.0") 
+
+install.packages("bbsBayes") # our package is now on CRAN
 library(bbsBayes)
 library(tidyverse)
 library(raster)
@@ -14,6 +16,7 @@ library(landscapemetrics)
 library(landscapetools)
 library(dplyr)
 library(rgdal)
+library(ggplot2)
 
 BBSroutes <- st_read("doc.kml")
 BBSroutes$Description  <- NULL
@@ -49,5 +52,16 @@ CCS2011 <- st_read(dsn = ".", layer = "2011CCS")
 
 CCS2016 <- st_read(dsn = ".", layer = "2016CCS")
 
-overlay1991 <- overlay(BBSroutes, CCS1991, fun=function(x, y){return(x*y)})
-plot(overlay1991)
+# overlay1991 <- overlay(BBSroutes, CCS1991, fun=function(x, y){return(x*y)})
+# plot(overlay1991)
+
+overlay1991 <- st_union(BBSroutes,CCS1991)
+### assuming the projections are correctly interpreted and recorded in the files, this should work
+
+## some useful guidance on plotting sf objects
+#https://www.r-spatial.org/r/2018/10/25/ggplot2-sf.html 
+
+
+plot.test = ggplot()+
+  geom_sf(data = BBSroutes)+
+  geom_sf(data = CCS1991,colour = "red")
